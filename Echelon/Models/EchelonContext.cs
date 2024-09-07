b5180ced5e5445cb8d2f466206bb3fce79b3f6fa
awesome-sparsh <sparshsethi@outlook.com>
2024-09-07 16:14:18 +0530

    initial commit

--- Echelon/Models/EchelonContext.cs
+++ Echelon/Models/EchelonContext.cs
@@ -15,7 +15,10 @@ public partial class EchelonContext : DbContext
     {
     }
 
+    public virtual DbSet<Inventory> Inventories { get; set; }
+
     public virtual DbSet<User> Users { get; set; }
+
     /*
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
@@ -23,6 +26,38 @@ public partial class EchelonContext : DbContext
     */
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Inventory>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("inventory");
+
+            entity.Property(e => e.Id).HasColumnType("int(11)");
+            entity.Property(e => e.CostPrice)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)")
+                .HasColumnName("Cost_price");
+            entity.Property(e => e.Discount)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)");
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(255)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnName("product_name");
+            entity.Property(e => e.Profit)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)")
+                .HasColumnName("profit");
+            entity.Property(e => e.SellPrice)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)")
+                .HasColumnName("Sell_price");
+            entity.Property(e => e.SerialNo)
+                .HasDefaultValueSql("'NULL'")
+                .HasColumnType("int(11)")
+                .HasColumnName("Serial_no");
+        });
+
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.Uid).HasName("PRIMARY");
---
+++ Echelon/Models/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Echelon.Models;
+
+public partial class Inventory
+{
+    public int Id { get; set; }
+
+    public int? SerialNo { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public int? CostPrice { get; set; }
+
+    public int? SellPrice { get; set; }
+
+    public int? Discount { get; set; }
+
+    public int? Profit { get; set; }
+}
